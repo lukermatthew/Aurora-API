c6e8653fd386450ce19af5c2b9a40febd062316c
Mat <matt.tan@aiqueinnovation.com>
2024-06-05 13:46:56 +0800

    first commit

---
+++ models/video.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+// const validator = require('validator');
+
+const videoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please tell us the title!']
+    },
+    thumbnail: {
+      type: String,
+      required: [true, 'Thumbnail is required!']
+    },
+    prompt: {
+      type: String,
+      required: [true, 'Prompt is required!']
+    },
+    video: {
+      type: String,
+      required: [true, 'Video is required!']
+    },
+
+    creator: {
+      type: String,
+      required: [true, 'A Creator is required!']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: true
+    }
+  }
+  // {
+  //   toJSON: { virtuals: true },
+  //   toObject: { virtuals: true }
+  // }
+);
+
+const Video = mongoose.model('Videos', videoSchema);
+
+module.exports = Video;
