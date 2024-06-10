8cf92505b0aed515488f7b3526b9d1c6776d10da
Mat <matt.tan@aiqueinnovation.com>
2024-06-10 15:33:00 +0800

    Fixed: Update search query params

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -13,3 +13,30 @@ exports.getAllVideos = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.searchVideos = catchAsync(async (req, res, next) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Please provide a search query'
+    });
+  }
+
+  // Create a regex for case-insensitive search
+  const searchRegex = new RegExp(query, 'i');
+  const videos = await Video.find({ title: { $regex: searchRegex } }).populate(
+    'creator',
+    'username avatar'
+  );
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: videos.length,
+    data: {
+      videos
+    }
+  });
+});
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -4,4 +4,5 @@ const videoController = require('../controllers/videoController');
 const router = express.Router();
 
 router.route('/').get(videoController.getAllVideos);
+router.route('/search').get(videoController.searchVideos);
 module.exports = router;
